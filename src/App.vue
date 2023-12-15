8af17c365563a0c8eef6bbb87a3c1c7bc0273a0e
Hammad Afzal <hammadafzal05@gmail.com>
2023-12-15 19:03:34 +0000

    Create header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue'
+import ProductDesc from './components/ProductDesc'
+</script>
+
+<template>
+  <SiteNavigation />
+  <ProductDesc />
+</template>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <header
+    class="bg-white border-b-2 border-black-100 border-solid sm:w-full md:w-full lg:w-full h-20 py-12"
+  >
+    <nav class="flex flex-row items-center flex-1 bg-slate-20 text-white">
+      <img src="/media/assets/tbps-logo.webp" alt="logo" class="w-44 ml-44 -mt-8" />
+    </nav>
+  </header>
+</template>
